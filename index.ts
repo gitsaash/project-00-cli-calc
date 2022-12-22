e41dbffb5ce81999db109da36776c626ac0ad0c0
Saad Shamsi <114074040+saad-shamsi@users.noreply.github.com>
2022-12-23 02:14:03 +0500

    Add files via upload

--- index.ts
+++ index.ts
@@ -1,124 +1,67 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
 import inquirer from "inquirer";
 import chalk from "chalk";
 import chalkAnimation from "chalk-animation";
-import { type } from "os";
-const promise = () => {
-  return new Promise((res) => {
-    setTimeout(res, 1000);
-  });
+const msg = "Bht Shubkamnayen";
+const sleep = () => new Promise((res, rej) => setTimeout(res, 2000));
+const loadingMsg = async () => {
+  
+  const loading = chalkAnimation.radar  (`'Loading a game please wait....'`);
+
+  await load();
+  loading.stop();
+   
 };
-async function rainbowlights() {
-  const animationRainbow = chalkAnimation.rainbow(
-    "let's Calaculate the values!"
-  );
-  await promise();
-  animationRainbow.stop();
-  console.log(chalk.cyan` _____________________
-   |  _________________  |
-   | | JO           0. | |
-   | |_________________| |
-   |  ___ ___ ___   ___  |
-   | | 7 | 8 | 9 | | + | |
-   | |___|___|___| |___| |
-   | | 4 | 5 | 6 | | - | |
-   | |___|___|___| |___| |
-   | | 1 | 2 | 3 | | x | |
-   | |___|___|___| |___| |
-   | | . | 0 | = | | / | |
-   | |___|___|___| |___| |
-   |_____________________|`);
+async function welcomeMessage(cb: Function) {
+  const showMessage = chalkAnimation.rainbow(`"Welcome to Guessing A Nmber Game!!! ${msg}"`)
+  await sleep();
+  showMessage.stop();
+return cb()
+ 
 }
-await rainbowlights();
-async function askQuestion() {
-  const answers = await inquirer.prompt([
-    /* Pass your questions in here */
-    {
+const load = () => new Promise((res, rej) => setTimeout(res, 3000));
 
-      type: "list",
-      name: "operator",
-      message:chalk.red ("which operation you want to perform? \n "),
-      choices:[
-        "Addition",
-        "Subtraction",
-        "Multiplication",
-        "Division",
-        "Modulus",
-        "Exponent",
-      ],
-    },
-    {
-      type: "number",
-      name: "Operand1",
-      message: "Enter 1st Value  :"
-      ,
-    },
-    {
-      type: "number",
-      name: "Operand2",
-      message: "Enter 2nd Value :",
-    },
-  ]);
-  if (answers.operator == "Addition") {
-    console.log(
-      `${answers.Operand1} + ${answers.Operand2} = ${
-        answers.Operand1 + answers.Operand2
-      }`
-    );
+welcomeMessage(loadingMsg);
+
+
+let playerLife = 3
+async function askQue() {
+  const randomNumber = Math.floor(Math.random() * 10 + 1)
+ do {
+  var que = await inquirer 
+  .prompt([{
+    type:"number",
+    name:"usr_num",
+    message:  "Guesss A Number Between 1-10 : ",
+    
+    validate: (answers:number) => {
+      if (isNaN(answers)){
+        return chalk.redBright("WRONG INPUT!!! Please enter a valid number")
+      
+      }
+      return true
+        
+      
+    }
+
+  }])
+  
+  console.log(que);
+  
+  
+  if(randomNumber=== que.usr_num){
+      console.log("CONGRATS! You Guessed The Correct NUmber");
+      
   }
-  if (answers.operator == "Subtraction") {
-    console.log(
-      `${answers.Operand1} - ${answers.Operand2} = ${
-        answers.Operand1 - answers.Operand2
-      }`
-    );
+  
+  if(randomNumber < que.usr_num){
+      console.log(`"HIGH! ${que.usr_num} Is Higher Than A Correct Number "`);
+      
   }
-  if (answers.operator == "Multiplication") {
-    console.log(
-      `${answers.Operand1} * ${answers.Operand2} = ${
-        answers.Operand1 * answers.Operand2
-      }`
-    );
+  if(randomNumber >que.usr_num){
+      console.log(`"LOW! ${que.usr_num}  Is Lower Than A Correct Number "`);
+      
   }
-  if (answers.operator == "Division") {
-    console.log(
-      `
-         ${answers.Operand1} / ${answers.Operand2} = ${
-        answers.Operand1 / answers.Operand2
-      }`
-    );
-  }
-  if (answers.operator == "Modulus") {
-    console.log(
-      `${answers.Operand1} % ${answers.Operand2} = ${
-        answers.Operand1 % answers.Operand2
-      }`
-    );
-  }
-  if (answers.operator == "Exponent") {
-    console.log(
-      `  The power of (${answers.Operand1} raise to the power  ${
-        answers.Operand2
-      } = ${answers.Operand1 ** answers.Operand2})`
-    );
-  }
+  
+} while (playerLife > 0 && randomNumber!== que.usr_num ); 
 }
-
-async function queForReUse() {
-  do {
-    await askQuestion();
-
-    var askingUser = await inquirer.prompt({
-      type: "input",
-      name: "re-calculate",
-      message: "Do you want to calculate more values? Enter y or n :",
-    });
-  } while (
-    askingUser["re-calculate"] === "y" ||
-    askingUser["re-calculate"] === "Y" ||
-    askingUser["re-calculate"] === "yes" ||
-    askingUser["re-calculate"] === "Yes"
-  );
-}
-queForReUse();
-let y= "saad"
